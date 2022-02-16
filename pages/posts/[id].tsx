ab62be8a002dd1c706a460d2e6ee786ce3e07663
Masayuki Onishi <omccello@gmail.com>
2022-02-17 01:00:50 +0900

    記事の空ページ作成

---
+++ pages/posts/[id].tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next';
+
+type Props = {};
+
+export const PostDetailPage: NextPage<Props> = (props) => {
+  const {} = props;
+
+  return <></>;
+};
+
+export default PostDetailPage;
---
+++ pages/posts/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next';
+
+type Props = {};
+
+export const AllPostPage: NextPage<Props> = (props) => {
+  const {} = props;
+
+  return <></>;
+};
+
+export default AllPostPage;
