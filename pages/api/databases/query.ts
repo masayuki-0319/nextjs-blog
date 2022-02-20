d28eb46dc916ccb414c6c74bd67c244f74922289
Masayuki Onishi <omccello@gmail.com>
2022-02-20 11:48:17 +0900

    api routes 中の NotionAPI ロジックを切り出し

--- pages/api/databases/query.ts
+++ pages/api/databases/query.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { Client } from '@notionhq/client';
+
+import { getDatabaseRecord } from './../../../lib/api/notion/get-database-record';
 
 interface DatabasesRequest extends NextApiRequest {}
 interface DatabasesResponse extends NextApiResponse {}
@@ -7,23 +8,9 @@ interface DatabasesResponse extends NextApiResponse {}
 const handler = async (req: DatabasesRequest, res: DatabasesResponse) => {
   try {
     if (req.method === 'GET') {
-      const NOTION_KEY = process.env.NOTION_KEY as string;
       const NOTION_DATABASE_ID = process.env.NOTION_DATABASE_ID as string;
 
-      const notion = new Client({
-        auth: NOTION_KEY,
-      });
-
-      const page_size = req.query.page_size ?? '10';
-
-      const databaseDetail = await notion.databases.query({
-        database_id: NOTION_DATABASE_ID,
-        page_size: Number(page_size),
-      });
-
-      const posts = databaseDetail.results;
-
-      const resonse = posts;
+      const resonse = await getDatabaseRecord({ databaseId: NOTION_DATABASE_ID });
 
       res.status(200).json(resonse);
       return;
